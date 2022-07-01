adcf646e75cb6a45d47f0ca5f0c742648aea1a9a
Dragon <alemfla3181@naver.com>
2022-07-01 14:37:15 +0900

    js basics #6

---
+++ javascript-practices/basics/ex11.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * Array 객체 함수 (Array.prototype.*)
+ * 
+ */
+
+var colors = ['black', 'white', 'yellow'];
+var fruits = ['apple', 'banana', 'mango'];
+
+// concat
+var a1 = fruits.concat(colors);
+console.log(a1);
