e7452d939e7e633b6062219872c42b1bbe40c37e
Dragon <alemfla3181@naver.com>
2022-07-11 14:52:45 +0900

    common.js

---
+++ javascript-practices/basics/module-ex04/error-node03.js
@@ -0,0 +1,8 @@
+module.exports = {
+  n: 30,
+  s: 'javascript module',
+  o: {}
+};
+
+exports.b = false;
+exports.f = function () {};
--- javascript-practices/basics/module-ex04/index.js
+++ javascript-practices/basics/module-ex04/index.js
@@ -1,3 +1,7 @@
 const mod01 = require('./error-node01');
+const mod02 = require('./error-node02');
+const mod03 = require('./error-node03');
 
 console.log(mod01);
+mod02();
+console.log(mod03);
