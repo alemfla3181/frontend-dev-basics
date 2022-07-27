dedfc922e44b6f70cdc607e4f397d56e59c24d9c
Dragon <alemfla3181@naver.com>
2022-07-27 10:24:44 +0900

    js async

--- javascript-practices/es6/ex09.mjs
+++ javascript-practices/es6/ex09.mjs
@@ -9,10 +9,22 @@ console.log(myArith.add(10, 20));
 console.log(myArith.subtract(10, 20));
 
 // 03
-// named export는 import 대상이 다 수이기 때문에 하나의 특정 이름으로 받을 수 없다.
+// named export는 import 대상이 다수이기 때문에 하나의 특정 이름으로 받을 수 없다.
 // 대신 * as ~ 를 사용해서 전체 import 대상을 하나의 객체로 받을 수 있다.
 import * as m from './ex09.03.mjs';
 console.log(m.add(10, 20));
-// import destructing
+// import destructing 1
 import {subtract} from './ex09.03.mjs';
 console.log(subtract(10, 20));
+
+// 04
+// import destructing 2
+import { add } from './ex09.04.mjs';
+console.log(add(10, 20));
+
+// 05
+import math2 from './ex09.05.mjs';
+console.log(math.divide(4, 2));
+console.log(math.multiply(4, 2));
+console.log(math2.add(4, 2));
+console.log(math2.subtract(4,2));
